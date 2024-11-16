e11c5a10a3952bb9a42f099975f9be81e1fe7546
FerosC101 <vincevillar02@gmail.com>
2024-11-16 11:54:26 +0800

    Start

--- src/service/authentication.py
+++ src/service/authentication.py
@@ -1,3 +1,20 @@
 class Authentication:
-    def __init__(self):
-        pass
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+
+    def login (self):
+        username = input("Enter your username: ")
+        password = input("Enter your password: ")
+
+    def authenticate_account(self, users):
+        if self.username in users:
+            print("Username already exists!")
+            return False
+        if len(self.username) < 4:
+            print("Username must be at least 4 characters!")
+            return False
+
+
+
